82794e3c301e428070ae5103344d6d828296b7f0
SulekSulowski <126951489+SulekSulowski@users.noreply.github.com>
2023-05-25 23:06:34 +0200

    Add files via upload

---
+++ Parzysta.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Kalkulator:
+    def parzysta(self, n): # sprawdzenie czy liczba n jest liczbą parzystą
+        if n % 2 == 0:
+            return "Liczba jest liczbą parzystą"
+        else:
+            return "Liczba nie jest liczbą parzystą"
+
+
